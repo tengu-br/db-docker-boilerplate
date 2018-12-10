0597226eaaa20549b6ac0d5d29404de64999b459
tengu-br <36267811+tengu-br@users.noreply.github.com>
2018-12-10 18:07:43 -0200

    Parcela (#1)
    * [RMV] codigo desnecessario

--- magica.php
+++ magica.php
@@ -7,13 +7,11 @@ $db =  [
     'dbname' => 'valoresResponsaveis',
 ];
 
-$user = 'sa';
-$password = 'now-tcdf';
 
 try {
     $pdo = new PDO('dblib:host=' . $db['host'] . ';dbname=' . $db['dbname'],
         $db['user'], $db['pass']);
-    echo "lol\n";
+    echo "conectado\n";
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
