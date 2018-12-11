39392f065890fb589584a6b68e31a2439c670391
tengu-br <vitorrcosta96@gmail.com>
2018-12-11 17:56:38 -0200

    [new] uuid

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,18 @@ FROM php:fpm-alpine
 #ADD PACKAGES
 RUN apk update \
 	&& apk add\
+	    curl\
         freetds-dev && \
         rm -rf /var/cache/apk/*
 
 #PHP EXTENSIONS
 RUN docker-php-ext-configure pdo_dblib \
     && docker-php-ext-install pdo_dblib
+
+#COMPOSER
+#RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+#RUN composer require ramsey/uuid
+#RUN composer install
 
 #COPY FILES
 COPY . ./
--- magica.php
+++ magica.php
@@ -1,17 +1,41 @@
 <?php
 
-$db =  [
-    'host' => '',
-    'user' => '',
-    'pass' => '',
-    'dbname' => '',
-];
+require 'vendor/autoload.php';
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
 
+//    Connect
+$db =  [
+    'host' => '10.9.16.220',
+    'user' => 'sa',
+    'pass' => 'now-tcdf',
+    'dbname' => 'valoresResponsaveis',
+];
 
 try {
     $pdo = new PDO('dblib:host=' . $db['host'] . ';dbname=' . $db['dbname'],
         $db['user'], $db['pass']);
     echo "conectado\n";
 } catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
+    echo 'erro conexao banco: ' . $e->getMessage();
+}
+
+// Select all
+$sql="SELECT * FROM valoresResponsaveis.dbo.testefred";
+$all= $pdo->query($sql);
+$result = $all->fetchAll();
+
+foreach($result as $row) {
+//    Generate
+    try {
+        $uuid4 = Uuid::uuid4();
+    } catch (UnsatisfiedDependencyException $e) {
+        echo 'Caught ramsey exception: ' . $e->getMessage() . "\n";
+    }
+//    Update
+    $cpf = $row["cpf"];
+    $sql="UPDATE valoresResponsaveis.dbo.testefred SET UUID='$uuid4' WHERE cpf='$cpf'";
+    $pdo->query($sql);
+//    Print
+    echo $row["cpf"] . "-" . $row["UUID"]. "\n";
 }
