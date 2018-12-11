10c3bbacc8dc27d04b85380a336385938606d21e
tengu-br <36267811+tengu-br@users.noreply.github.com>
2018-12-11 14:03:44 -0200

    Parcela (#2)
    * [RMV] codigo desnecessario
    
    * [RMV] codigo desnecessario
    
    * [RMV] codigos desnecessários
    
    * [RMV] codigo desnecessario

--- docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-composer install --prefer-dist --no-dev --no-progress --no-suggest --optimize-autoloader --classmap-authoritative --no-interaction
-
-exec docker-php-entrypoint "$@"
