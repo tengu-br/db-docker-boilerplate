c6fdbfa9ec8084dfd08f2d680e032fec3621a3c7
madureiradaniel <madureiradaniel600@gmail.com>
2018-12-20 17:54:07 -0200

    [ADD] Finalizado

--- migracao.php
+++ migracao.php
@@ -23,45 +23,7 @@ try {
 
 $pdo->setAttribute(PDO::DBLIB_ATTR_STRINGIFY_UNIQUEIDENTIFIER, true);
 
-// $sql = "SELECT * FROM valoresResponsaveis.dbo.ValorProcesso";
-// $return = $pdo->query($sql);
-// $query = $return->fetchAll();
-
-// foreach ($query as $row) {
-
-//     $id = $row["idProcesso"];
-
-//     $sql2 = "SELECT * FROM valoresResponsaveis.dbo.ValorProcesso WHERE idProcesso = $id";
-//     $return2 = $pdo->query($sql);
-//     $query2 = $return2->fetchAll();
-//     $flag3 = 0;
-//     $flag14 = 0;
-
-//     foreach ($query2 as $row2) {
-
-//         if ($row2["idTipoValor"] == 3) {
-
-//             $flag3 = 1;
-//         }
-//         if ($row2["idTipoValor"] == 14 || $row["idTipoValor"] == 16 || $row["idTipoValor"] == 17) {
-
-//             $flag14 = 1;
-//         }
-
-//     }
-//     if($flag3 ==1 && $flag14 ==1)
-//     {
-
-//         echo $row["idValorProcesso"] . "\n";
-//     }
-
-// }
-
-// die();
-
-// $pdo->query("TRUNCATE TABLE debitos.dbo.imputacao");
-
-$sql = "SELECT * FROM valoresResponsaveis.dbo.ValorProcesso ";
+$sql = "SELECT * FROM valoresResponsaveis.dbo.ValorProcesso";
 $return = $pdo->query($sql);
 $query = $return->fetchAll();
 
@@ -70,23 +32,29 @@ foreach ($query as $row) {
     if ($row["idTipoValor"] == 3 || $row["idTipoValor"] == 14 || $row["idTipoValor"] == 16 || $row["idTipoValor"] == 17) {
 
         $flag_registro_novo = 1;
-        $idNatureza = "adcfae94-7641-4b4f-abb7-36c8fe8e1a4f"; //tudo debito
-        $idSituacao = "0BD297A4-0776-41F8-A060-4982A3943D6B"; // Apuração, inadiplente, imputação, quitado, Em cobrança executiva !!!!! pendente !!!!!
+        $edoc = 0;
+        $numProcesso = 0;
+        $ano_processo = 0;
+        $valor_apurado = 0;
+        $valor_imputado = 0;
+
+        //multa tem apuração ?
+        //da maneira atual multa é só "valor imputado"
+        if ($row["idTipoValor"] == 3 || $row["idTipoValor"] == 16 || $row["idTipoValor"] == 17) {
+            $idNatureza = "adcfae94-7641-4b4f-abb7-36c8fe8e1a4f"; //tudo debito
+        } else {
+            $idNatureza = "BEF3D7D5-EF28-4CE2-AA7E-94E30FA9B8B9"; //tudo multa
+        }
 
         // $IdImputacao = Uuid::uuid4();
         $IdImputacao = $row["UUID"]; //id imputação uuid
 
         $idValorProcesso = $row["idValorProcesso"];
-        $data_imputacao = $row["dtValor"];
 
         //pegar nr e ano pelo idProcesso, buscando no etcdf
         $sql_Valordecisao = "SELECT * FROM valoresResponsaveis.dbo.ValorDecisao WHERE idValorProcesso = $idValorProcesso";
         $query1 = $pdo->query($sql_Valordecisao);
         $query_ValorDecisao = $query1->fetchAll();
-
-        $edoc = 0;
-        $numProcesso = 0;
-        $ano_processo = 0;
 
         foreach ($query_ValorDecisao as $row_decisao) {
 
@@ -136,52 +104,140 @@ foreach ($query as $row) {
             $flag_apuracao = 0;
         }
 
-        //vendo se já existe um uuid do processo no banco novo, se sim, ele usa o uuid existente
-        $idProcesso = $row["idProcesso"];
+        // vendo se já existe um uuid do processo no banco novo, se sim, ele usa o uuid existente
+        // $sql_idProcesso = "SELECT * FROM valoresResponsaveis.dbo.ValorProcesso WHERE idProcesso = $idProcesso";
+        // $return_idprocesso = $pdo->query($sql_idProcesso);
+        // $query_idProcesso = $return_idprocesso->fetchAll();
 
-        $sql_idProcesso = "SELECT * FROM valoresResponsaveis.dbo.ValorProcesso WHERE idProcesso = $idProcesso";
-        $return_idprocesso = $pdo->query($sql_idProcesso);
-        $query_idProcesso = $return_idprocesso->fetchAll();
+        // $idProcesso = $row["idProcesso"];
 
-        foreach ($query_idProcesso as $row_processo) {
-            if (($row_processo["idValorProcesso"] < $row["idValorProcesso"]) && ($row_processo["idTipoValor"] == 3 || $row_processo["idTipoValor"] == 14 || $row_processo["idTipoValor"] == 16 || $row_processo["idTipoValor"] == 17)) {
-                $IdImputacao = $row_processo["UUID"];
-                // echo $row_processo["idValorProcesso"] . "|". $row["idValorProcesso"] . "\n";
-                $flag_registro_novo = 0;
-                break;
-            }
+        // foreach ($query_idProcesso as $row_processo) {
+        //     if (($row_processo["idValorProcesso"] < $row["idValorProcesso"]) && ($row_processo["idTipoValor"] == 3 || $row_processo["idTipoValor"] == 14 || $row_processo["idTipoValor"] == 16 || $row_processo["idTipoValor"] == 17)) {
+        //         $IdImputacao = $row_processo["UUID"];
+        //         $flag_registro_novo = 0;
+        //         break;
+        //     }
+        // }
+
+        //acumulando valor de multiplas multas / debitos
+
+        // foreach ($query_idProcesso as $row_processo) {
+        //     if ($row_processo["idTipoValor"] == 3 && $row_processo["idTipoValor"] == $row["idTipoValor"]) {
+        //         $valor_apurado = $valor_apurado + $row_processo["valor"];
+        //         echo $valor_apurado . "\n";
+        //     } elseif ($row_processo["idTipoValor"] == 14 && $row_processo["idTipoValor"] == $row["idTipoValor"]) {
+        //         $valor_imputado = $valor_imputado + $row_processo["valor"];
+        //     } elseif (($row_processo["idTipoValor"] == 16 || $row_processo["idTipoValor"] == 17) && (17 == $row["idTipoValor"] || 16 == $row["idTipoValor"])) {
+        //         $valor_imputado = $valor_imputado + $row_processo["valor"];
+        //         echo $valor_imputado . "\n";
+        //     }
+        // }
+
+        //situação
+        $idSituacao = null; // Apuração, inadiplente, imputação, quitado, Em cobrança executiva !!!!!
+
+        // $sql_recuperado = "SELECT * FROM valoresResponsaveis.dbo.ValorProcesso  WHERE idProcesso = $idProcesso";
+        // $return_recuperado = $pdo->query($sql_recuperado);
+        // $query_recuperado = $return_recuperado->fetchAll();
+
+        $valor_recuperado = 0;
+        $data_imputacao = 0;
+
+        //hoje
+        $today = getdate();
+        $datetimeFormat = 'Y-m-d H:i:s';
+        $date = new \DateTime();
+        $date->setTimestamp($today[0]);
+        $aux = $date->format($datetimeFormat);
+
+        $sql_valor = "SELECT * FROM valoresResponsaveis.dbo.ValorRecuperado  WHERE idValorProcesso = $idValorProcesso";
+        $return_valor = $pdo->query($sql_valor);
+        $query_valor = $return_valor->fetchAll();
+
+        foreach ($query_valor as $row_valor) {
+            $valor_recuperado = $valor_recuperado + $row_valor["valor"];
         }
+
+        // foreach ($query_recuperado as $row_recuperado) {
+
+        //     if ($row_recuperado["dtValor"] > $data_imputacao) {
+        $data_imputacao = $row["dtValor"];
+        //     }
+
+        //     if ($row_recuperado["idTipoValor"] == 14 || $row_recuperado["idTipoValor"] == 16 || $row_recuperado["idTipoValor"] == 17) {
+
+        //     }
+
+        // }
+
+        //diferença em dias das duas datas
+        $ts1 = strtotime($data_imputacao);
+        $ts2 = strtotime($aux);
+        $diff = ($ts2 - $ts1) / 86400;
+
+        if ($valor_imputado == null) {
+            $idSituacao = "0BD297A4-0776-41F8-A060-4982A3943D6B"; //apuração
+        } elseif ($valor_recuperado >= $valor_imputado) {
+            $idSituacao = "EBF385AF-DBD4-4F14-A87F-C65BDF96C543"; //quitado
+        } elseif ($diff <= 30) {
+            $idSituacao = "0E5C9CB6-ADFA-4467-B78B-6A61B2A50DFE"; //imputação
+        } else {
+            $idSituacao = "D062B2EC-FBD0-4FA3-857F-590B04756369"; //inadimplente
+        }
+        // echo "<><><><><><>" .$idSituacao . "\n";
 
         // echo $IdImputacao."|".$idValorProcesso."|".$numProcesso."|".$ano_processo."|".$data_imputacao."|".$valor_imputado."|".$edoc."|".$prazo."|".$data_notificacao."|".$idNatureza."|".$idSituacao."|". "\n";
         //registros finais
 
         if ($flag_apuracao == 1 && $flag_registro_novo == 1) {
             //insert se é registro novo e apuracao
-            $sql_query_debitos = "INSERT INTO debitos.dbo.imputacao (idImputacao, numProcesso,ano_processo,data_imputacao, valor_apurado, edoc, prazo,data_notificacao, idNatureza, idSituacao ) " .
-                "VALUES ('$IdImputacao','$numProcesso','$ano_processo','$data_imputacao','$valor_apurado' ,'$edoc','$prazo','$data_notificacao','$idNatureza','$idSituacao')";
-            $pdo->query($sql_query_debitos);
-            //  echo $sql_query_debitos ."\n";
-            echo "INSERT apuração: " . "UUID: " . $idValorProcesso . " valor: " . $valor_apurado . "\n";
+            if ($data_notificacao == null) {
+                $sql_query_debitos = "INSERT INTO debitos.dbo.imputacao (idImputacao, numProcesso,ano_processo,data_imputacao, valor_apurado, edoc, prazo, idNatureza, idSituacao ) " .
+                    "VALUES ('$IdImputacao','$numProcesso','$ano_processo','$data_imputacao','$valor_apurado' ,'$edoc','$prazo','$idNatureza','$idSituacao')";
+                $pdo->query($sql_query_debitos);
+                echo $sql_query_debitos . "\n";
+
+            } else {
+
+                $sql_query_debitos = "INSERT INTO debitos.dbo.imputacao (idImputacao, numProcesso,ano_processo,data_imputacao, valor_apurado, edoc, prazo,data_notificacao, idNatureza, idSituacao ) " .
+                    "VALUES ('$IdImputacao','$numProcesso','$ano_processo','$data_imputacao','$valor_apurado' ,'$edoc','$prazo','$data_notificacao','$idNatureza','$idSituacao')";
+                $pdo->query($sql_query_debitos);
+                echo $sql_query_debitos . "\n";
+
+            }
+            // echo "INSERT apuração: " . "UUID: " . $idValorProcesso . " valor: " . $valor_apurado . "\n";
         } elseif ($flag_apuracao == 1 && $flag_registro_novo == 0) {
+
             //insert se é registro existente e apuracao
-            $sql_query_debitos = "UPDATE debitos.dbo.imputacao SET valor_apurado= $valor_apurado WHERE idImputacao = '$IdImputacao' ";
+            $sql_query_debitos = "UPDATE debitos.dbo.imputacao SET valor_apurado= $valor_apurado,idSituacao = '$idSituacao' WHERE idImputacao = '$IdImputacao' ";
             $pdo->query($sql_query_debitos);
             echo $sql_query_debitos . "\n";
-            echo "UPDATE apuração: " . "UUID: " . $idValorProcesso . " valor: " . $valor_apurado . "\n";
+            // echo "UPDATE apuração: " . "UUID: " . $idValorProcesso . " valor: " . $valor_apurado . "\n";
 
         } elseif ($flag_apuracao == 0 && $flag_registro_novo == 1) {
             //insert se é registro novo e imputacao
-            $sql_query_debitos = "INSERT INTO debitos.dbo.imputacao (idImputacao, numProcesso,ano_processo,data_imputacao, valor_imputado, edoc, prazo,data_notificacao, idNatureza, idSituacao ) " .
-                "VALUES ('$IdImputacao','$numProcesso','$ano_processo','$data_imputacao','$valor_imputado','$edoc','$prazo','$data_notificacao','$idNatureza','$idSituacao')";
-            $pdo->query($sql_query_debitos);
-            // echo $sql_query_debitos."\n";
-            echo "INSERT imputação: " . "UUID: " . $idValorProcesso . " valor: " . $valor_imputado . "\n";
+
+            if ($data_notificacao == null) {
+                $sql_query_debitos = "INSERT INTO debitos.dbo.imputacao (idImputacao, numProcesso,ano_processo,data_imputacao, valor_imputado, edoc, prazo, idNatureza, idSituacao ) " .
+                    "VALUES ('$IdImputacao','$numProcesso','$ano_processo','$data_imputacao','$valor_imputado','$edoc','$prazo','$idNatureza','$idSituacao')";
+                $pdo->query($sql_query_debitos);
+                echo $sql_query_debitos . "\n";
+
+            } else {
+
+                $sql_query_debitos = "INSERT INTO debitos.dbo.imputacao (idImputacao, numProcesso,ano_processo,data_imputacao, valor_imputado, edoc, prazo,data_notificacao, idNatureza, idSituacao ) " .
+                    "VALUES ('$IdImputacao','$numProcesso','$ano_processo','$data_imputacao','$valor_imputado','$edoc','$prazo','$data_notificacao','$idNatureza','$idSituacao')";
+                $pdo->query($sql_query_debitos);
+                echo $sql_query_debitos . "\n";
+
+            }
+            // echo "INSERT imputação: " . "UUID: " . $idValorProcesso . " valor: " . $valor_imputado . "\n";
         } else {
             //insert se é registro existente e imputacao
-            $sql_query_debitos = "UPDATE debitos.dbo.imputacao SET valor_imputado = $valor_imputado WHERE idImputacao = '$IdImputacao' ";
+            $sql_query_debitos = "UPDATE debitos.dbo.imputacao SET valor_imputado = $valor_imputado, idSituacao = '$idSituacao' WHERE idImputacao = '$IdImputacao' ";
             $pdo->query($sql_query_debitos);
             echo $sql_query_debitos . "\n";
-            echo "UPDATE imputação: " . "UUID: " . $idValorProcesso . " valor: " . $valor_imputado . "\n";
+            // echo "UPDATE imputação: " . "UUID: " . $idValorProcesso . " valor: " . $valor_imputado . "\n";
         }
 
     } else {
@@ -191,3 +247,5 @@ foreach ($query as $row) {
     }
 
 }
+
+
